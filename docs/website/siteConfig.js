59cc5ffbdf533b74246d4a40b8213b9f59102b55
José Gabriel <gabrielnovais@incorptech.com.br>
2020-02-26 16:31:56 -0300

    #2 creating a especific route to lucid examples

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -38,7 +38,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'installation', label: 'Docs'},
+    {doc: 'to_do_list', label: 'Docs'},
+    {doc: 'to_do_list', label: 'Examples'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
